25ab48159fdf21dd200f90afa446acc3c1b5cb4a
Dimitar Bounov <dimitar.bounov@consensys.net>
2023-07-17 16:04:48 -1000

    Support expectRevert for CREATE, CREATE2, STATICCALL, DELEGATECALL, CALLCODE; Add relevant tests

--- src/debug/opcode_interposing.ts
+++ src/debug/opcode_interposing.ts
@@ -6,6 +6,7 @@ import {
     writeCallOutput
 } from "@ethereumjs/evm/dist/opcodes";
 import { AsyncDynamicGasHandler, SyncDynamicGasHandler } from "@ethereumjs/evm/dist/opcodes/gas";
+import { bigIntToBuffer } from "@ethereumjs/util";
 import { AddOpcode } from "@ethereumjs/evm/dist/types";
 import {
     FoundryCheatcodesAddress,
@@ -14,7 +15,7 @@ import {
     returnStateMatchesRevert
 } from "./foundry_cheatcodes";
 import { RunState } from "@ethereumjs/evm/dist/interpreter";
-import { Address } from "ethereumjs-util";
+import { Address, setLengthLeft } from "ethereumjs-util";
 import { Common } from "@ethereumjs/common";
 import { bigEndianBufToBigint } from "../utils";
 import { ERROR } from "@ethereumjs/evm/dist/exceptions";
@@ -84,6 +85,71 @@ export function foundryInterposedOps(opcodes: any, foundryCtx: FoundryContext): 
                 runState.stack.push(caller);
             }
         ],
+        // 0xf0: CREATE
+        [
+            0xf0,
+            async function (runState, common) {
+                // If we're calling vm.* ignore the expectedRevert
+                const expectedRevert = getExpectedRevert(foundryCtx);
+                const [value, offset, length] = runState.stack.popN(3);
+
+                if (
+                    common.isActivatedEIP(3860) &&
+                    length > Number(common.param("vm", "maxInitCodeSize")) &&
+                    !runState.interpreter._evm._allowUnlimitedInitCodeSize
+                ) {
+                    trap(ERROR.INITCODE_SIZE_VIOLATION);
+                }
+
+                const gasLimit = runState.messageGasLimit!;
+                runState.messageGasLimit = undefined;
+
+                let data = Buffer.alloc(0);
+                if (length !== BigInt(0)) {
+                    data = runState.memory.read(Number(offset), Number(length), true);
+                }
+
+                const ret = await runState.interpreter.create(gasLimit, value, data);
+                handleCreateReturn(runState, ret, expectedRevert);
+            }
+        ],
+        // 0xf5: CREATE2
+        [
+            0xf5,
+            async function (runState, common) {
+                const expectedRevert = foundryCtx.getExpectedRevert();
+
+                if (runState.interpreter.isStatic()) {
+                    trap(ERROR.STATIC_STATE_CHANGE);
+                }
+
+                const [value, offset, length, salt] = runState.stack.popN(4);
+
+                if (
+                    common.isActivatedEIP(3860) &&
+                    length > Number(common.param("vm", "maxInitCodeSize")) &&
+                    !runState.interpreter._evm._allowUnlimitedInitCodeSize
+                ) {
+                    trap(ERROR.INITCODE_SIZE_VIOLATION);
+                }
+
+                const gasLimit = runState.messageGasLimit!;
+                runState.messageGasLimit = undefined;
+
+                let data = Buffer.alloc(0);
+                if (length !== BigInt(0)) {
+                    data = runState.memory.read(Number(offset), Number(length), true);
+                }
+
+                const ret = await runState.interpreter.create2(
+                    gasLimit,
+                    value,
+                    data,
+                    setLengthLeft(bigIntToBuffer(salt), 32)
+                );
+                handleCreateReturn(runState, ret, expectedRevert);
+            }
+        ],
         // 0xf1: CALL
         [
             0xf1,
@@ -93,14 +159,7 @@ export function foundryInterposedOps(opcodes: any, foundryCtx: FoundryContext): 
 
                 const toAddress = new Address(addressToBuffer(toAddr));
 
-                const expectedRevert = toAddress.equals(FoundryCheatcodesAddress)
-                    ? undefined
-                    : foundryCtx.getExpectedRevert();
-
-                if (expectedRevert) {
-                    // We expected a revert - clear it
-                    foundryCtx.clearExpectRevert();
-                }
+                const expectedRevert = getExpectedRevert(foundryCtx, toAddress);
 
                 let data = Buffer.alloc(0);
                 if (inLength !== BigInt(0)) {
@@ -112,20 +171,92 @@ export function foundryInterposedOps(opcodes: any, foundryCtx: FoundryContext): 
 
                 const ret = await runState.interpreter.call(gasLimit, toAddress, value, data);
 
-                handleReturn(runState, ret, foundryCtx, outOffset, outLength, expectedRevert);
+                handleReturn(runState, ret, outOffset, outLength, expectedRevert);
             }
         ],
         // 0x00: STOP
         [
             0x00,
             function (runState) {
-                const expectedRevert = foundryCtx.getExpectedRevert();
+                const expectedRevert = getExpectedRevert(foundryCtx);
                 if (expectedRevert === undefined) {
                     trap(ERROR.STOP);
                 } else {
-                    foundryCtx.clearExpectRevert();
                     runState.interpreter.revert(Buffer.alloc(0));
                 }
+            }
+        ],
+        // 0xf2: CALLCODE
+        [
+            0xf2,
+            async function (runState: RunState) {
+                const [_currentGasLimit, toAddr, value, inOffset, inLength, outOffset, outLength] =
+                    runState.stack.popN(7);
+                const toAddress = new Address(addressToBuffer(toAddr));
+                const expectedRevert = getExpectedRevert(foundryCtx, toAddress);
+
+                const gasLimit = runState.messageGasLimit!;
+                runState.messageGasLimit = undefined;
+
+                let data = Buffer.alloc(0);
+                if (inLength !== BigInt(0)) {
+                    data = runState.memory.read(Number(inOffset), Number(inLength), true);
+                }
+
+                const ret = await runState.interpreter.callCode(gasLimit, toAddress, value, data);
+
+                handleReturn(runState, ret, outOffset, outLength, expectedRevert);
+            }
+        ],
+        // 0xf4: DELEGATECALL
+        [
+            0xf4,
+            async function (runState) {
+                const value = runState.interpreter.getCallValue();
+                const [_currentGasLimit, toAddr, inOffset, inLength, outOffset, outLength] =
+                    runState.stack.popN(6);
+                const toAddress = new Address(addressToBuffer(toAddr));
+                const expectedRevert = getExpectedRevert(foundryCtx, toAddress);
+
+                let data = Buffer.alloc(0);
+                if (inLength !== BigInt(0)) {
+                    data = runState.memory.read(Number(inOffset), Number(inLength), true);
+                }
+
+                const gasLimit = runState.messageGasLimit!;
+                runState.messageGasLimit = undefined;
+
+                const ret = await runState.interpreter.callDelegate(
+                    gasLimit,
+                    toAddress,
+                    value,
+                    data
+                );
+
+                handleReturn(runState, ret, outOffset, outLength, expectedRevert);
+            }
+        ],
+        // 0xfa: STATICCALL
+        [
+            0xfa,
+            async function (runState) {
+                const value = BigInt(0);
+                const [_currentGasLimit, toAddr, inOffset, inLength, outOffset, outLength] =
+                    runState.stack.popN(6);
+                const toAddress = new Address(addressToBuffer(toAddr));
+                const expectedRevert = getExpectedRevert(foundryCtx, toAddress);
+
+                const gasLimit = runState.messageGasLimit!;
+                runState.messageGasLimit = undefined;
+
+                let data = Buffer.alloc(0);
+                if (inLength !== BigInt(0)) {
+                    data = runState.memory.read(Number(inOffset), Number(inLength), true);
+                }
+
+                const ret = await runState.interpreter.callStatic(gasLimit, toAddress, value, data);
+
+                handleReturn(runState, ret, outOffset, outLength, expectedRevert);
             }
         ],
         // 0xf3: RETURN
@@ -136,9 +267,7 @@ export function foundryInterposedOps(opcodes: any, foundryCtx: FoundryContext): 
                 let returnData = Buffer.alloc(0);
 
                 // If we're calling vm.* ignore the expectedRevert
-                const expectedRevert = runState.env.address.equals(FoundryCheatcodesAddress)
-                    ? undefined
-                    : foundryCtx.getExpectedRevert();
+                const expectedRevert = getExpectedRevert(foundryCtx);
 
                 if (expectedRevert === undefined) {
                     if (length !== BigInt(0)) {
@@ -146,7 +275,6 @@ export function foundryInterposedOps(opcodes: any, foundryCtx: FoundryContext): 
                     }
                     runState.interpreter.finish(returnData);
                 } else {
-                    foundryCtx.clearExpectRevert();
                     runState.interpreter.revert(returnData);
                 }
             }
@@ -160,10 +288,23 @@ export function foundryInterposedOps(opcodes: any, foundryCtx: FoundryContext): 
     return res;
 }
 
+function getExpectedRevert(ctx: FoundryContext, toAddress?: Address): RevertMatch {
+    const expectedRevert =
+        toAddress && toAddress.equals(FoundryCheatcodesAddress)
+            ? undefined
+            : ctx.getExpectedRevert();
+
+    if (expectedRevert) {
+        // We expected a revert - clear it
+        ctx.clearExpectRevert();
+    }
+
+    return expectedRevert;
+}
+
 function handleReturn(
     runState: RunState,
     ret: bigint,
-    ctx: FoundryContext,
     outOffset: bigint,
     outLength: bigint,
     expectedRevert: RevertMatch
@@ -182,5 +323,19 @@ function handleReturn(
     }
 
     writeCallOutput(runState, outOffset, outLength);
+    runState.stack.push(ret);
+}
+
+function handleCreateReturn(runState: RunState, ret: bigint, expectedRevert: RevertMatch): void {
+    if (expectedRevert === undefined) {
+        // Nothing to do - no expected revert
+    } else if (!returnStateMatchesRevert(ret, runState, expectedRevert)) {
+        // If return doesn't match expected revert (no revert, or different bytes/signature) throw an error
+        ret = 0n;
+    } else {
+        // Otherwise we match the expected revert, so return all 1s
+        ret = 1n;
+    }
+
     runState.stack.push(ret);
 }
---
+++ test/samples/local/foundry_expectRevert_otherCalls/contracts/test/expectRevert.t.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract Bomb {
+	constructor() public {
+		revert();
+	}
+}
+
+contract Bomb2 {
+	constructor() public {
+	}
+
+    function boom() public view returns (uint256){
+        revert();
+    }
+
+    function noBoom() public view returns (uint256){
+        return 1;
+    }
+}
+
+library LibBomb {
+    function boom() external view returns (uint256){
+        revert();
+    }
+
+    function noBoom() external view returns (uint256){
+        return 1;
+    }
+}
+
+contract Test_expectRevertOtherCalls is Test {
+    Bomb2 b;
+    event AssertionFailed(string message);
+
+    error E1();
+    error E2(string m);
+
+    uint256 private c;
+
+    constructor() {
+        b = new Bomb2();
+    }
+
+    function test_expectRevertNested_0() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try new Bomb() returns (Bomb b) {
+            assert(false); //should get here
+        } catch {
+            return 3;
+        }
+    }
+
+    function test_expectRevertNested_1() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try new Bomb{salt: bytes32(uint(1))}() returns (Bomb b) {
+            assert(false); //should get here
+        } catch {
+            return 3;
+        }
+    }
+
+    function test_expectRevertNested_2() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try new Bomb{salt: bytes32(uint(1))}() returns (Bomb b) {
+            assert(false); //should get here
+        } catch {
+            return 3;
+        }
+    }
+
+    function test_expectRevertNested_3() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try b.boom() returns (uint256 x) {
+            assert(false); //should get here
+        } catch {
+            return 3;
+        }
+    }
+
+    function test_expectRevertNested_4() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try b.noBoom() returns (uint256 x) {
+            return 3;
+        } catch {
+            assert(false); //should get here
+        }
+    }
+
+/*
+    function test_expectRevertNested_5() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try LibBomb.boom() returns (uint256 x) {
+            assert(false); //should get here
+        } catch {
+            return 3;
+        }
+    }
+
+    function test_expectRevertNested_6() external returns (uint256) {
+        vm.expectRevert(); // Expected to fail
+        try LibBomb.noBoom() returns (uint256 x) {
+            return 3;
+        } catch {
+            assert(false); //should get here
+        }
+    }
+    */
+}
